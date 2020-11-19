60df5dabdc6ab0c65e24c3f2c8ce05df5366bad9
Luiz Aoqui <luiz@hashicorp.com>
2020-11-19 11:57:14 -0500

    add nomad config node and update job node

--- nomad/nomad.js
+++ nomad/nomad.js
@@ -1,33 +1,60 @@
 const Nomad = require("../lib/nomad-api-client");
 
 module.exports = function (RED) {
-  function GetJob(config) {
+  function NomadClientNode(config) {
     RED.nodes.createNode(this, config);
 
-    let node = this;
-    let nomad = new Nomad();
+    this.address = config.address;
+    this.region = config.region;
+    this.namespace = config.namespace;
+
+    this.client = new Nomad({
+      address: this.address,
+      region: this.region,
+      namespace: this.namespace,
+    });
+  }
+  RED.nodes.registerType("nomad-client", NomadClientNode);
+
+  function NomadGetJobNode(config) {
+    RED.nodes.createNode(this, config);
+
+    this.nomad = RED.nodes.getNode(config.client);
+    this.jobId = config.jobId;
+    let sub = null;
+
+    const node = this;
 
     this.handleData = function (data) {
+      let color = "grey";
+      switch (data.Status) {
+        case "running":
+          color = "green";
+          break;
+        case "dead":
+          color = "red";
+          break;
+      }
+      node.status({ fill: color, shape: "dot", text: data.Status });
       node.send({ payload: data });
     };
 
     this.handleError = function (err) {
-      console.error(err);
+      node.error(err, "failed to get job");
     };
 
     this.on("close", function (removed, done) {
       if (node.sub) {
-        sub.removeAllListeners();
+        node.sub.removeAllListeners();
+        node.sub = null;
       }
       done();
     });
 
-    if (config.jobId) {
-      this.sub = nomad.jobs.get(config.jobId, { blocking: true });
-      this.sub.on("data", this.handleData);
-      this.sub.on("error", this.handleError);
-    }
+    this.sub = this.nomad.client.jobs.get(config.jobId, { blocking: true });
+    this.sub.on("data", this.handleData);
+    this.sub.on("error", this.handleError);
   }
 
-  RED.nodes.registerType("nomad-job", GetJob);
+  RED.nodes.registerType("nomad-get-job", NomadGetJobNode);
 };
